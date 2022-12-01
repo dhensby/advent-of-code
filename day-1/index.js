cf234ee67fe17635b46d2617baaa5a61bee068e8
Daniel Hensby <git@hens.by>
2022-12-01 08:52:15 +0200

    day 1 complete

---
+++ day-1/index.js
@@ -0,0 +1,35 @@
+const readFileLines = require('../utils');
+
+(async () => {
+    const data = await readFileLines('./input.txt');
+    // work out the totals for each group in the input
+    const totals = data.reduce((sums, next) => {
+        if (!next) {
+            sums.push(0);
+        } else {
+            const val = parseInt(next, 10);
+            const i = sums.length - 1;
+            sums[i] += val;
+        }
+        return sums;
+    }, []);
+    // find the top 3 from the totals - this could be done lazily with a sort then slice, but this is more fun :)
+    const top3 = totals.reduce((maxima, next) => {
+        // iterate through all the maxima and find the index at which
+        // the next number is larger. If it's found, then insert the
+        // value at that index and pop off the last item (keeping a fixed
+        // length of array)
+        for (let i = 0; i < maxima.length; i++) {
+            // @todo - it would probably be less iterations if we went backwards
+            // through the array checking that the value was not less than the
+            // last item
+            if (next > maxima[i]) {
+                maxima.splice(i, 0, next);
+                maxima.pop();
+                break;
+            }
+        }
+        return maxima;
+    }, [0, 0, 0]);
+    console.log(top3[0], top3.reduce((sum, next) => sum + next, 0));
+})().catch(console.error);
---
+++ utils.js
@@ -0,0 +1,6 @@
+const { readFile } = require('fs').promises;
+
+module.exports = async function readFileLines(path) {
+    const data = await readFile(path);
+    return data.toString().split('\n');
+}
