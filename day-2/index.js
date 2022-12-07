eee7e3547a147790cadf262d4cbfeeb3f7e01281
Daniel Hensby <git@hens.by>
2022-12-07 18:03:20 +0200

    Add linting and github action to enforce

--- day-2/index.js
+++ day-2/index.js
@@ -3,71 +3,77 @@ const OPPONENT_MOVES = ['A', 'B', 'C'];
 const PLAYER_MOVES = ['X', 'Y', 'Z'];
 
 function roundResult(opponent, player) {
-    const playerScore = PLAYER_MOVES.indexOf(player);
-    const opponentScore = OPPONENT_MOVES.indexOf(opponent);
-    // draw
-    if (playerScore === opponentScore) {
-        return 0;
-    }
-    // we have a non-transitive game here! Whoop!
-    // each item beats the "next" one and the "next" one cycles
-    // we can use some MODULAR ARITHMATIC to cycle through the options :)
-    const losesTo = (playerScore + 1) % OPPONENT_MOVES.length;
-    // lose
-    if (opponentScore === losesTo) {
-        return -1;
-    }
-    // win
-    return 1;
+  const playerScore = PLAYER_MOVES.indexOf(player);
+  const opponentScore = OPPONENT_MOVES.indexOf(opponent);
+  // draw
+  if (playerScore === opponentScore) {
+    return 0;
+  }
+  // we have a non-transitive game here! Whoop!
+  // each item beats the "next" one and the "next" one cycles
+  // we can use some MODULAR ARITHMATIC to cycle through the options :)
+  const losesTo = (playerScore + 1) % OPPONENT_MOVES.length;
+  // lose
+  if (opponentScore === losesTo) {
+    return -1;
+  }
+  // win
+  return 1;
 }
 
 function roundMove(opponent, result) {
-    const opponentScore = OPPONENT_MOVES.indexOf(opponent);
-    switch (result) {
-        case 'X':
-            // lose
-            return opponentScore === 0 ? PLAYER_MOVES.length - 1 : opponentScore - 1
-        case 'Y':
-            return opponentScore;
-        case 'Z':
-            return (opponentScore + 1) % PLAYER_MOVES.length;
-    }
+  const opponentScore = OPPONENT_MOVES.indexOf(opponent);
+  switch (result) {
+    case 'X':
+      // lose
+      return opponentScore === 0 ? PLAYER_MOVES.length - 1 : opponentScore - 1;
+    case 'Y':
+      return opponentScore;
+    case 'Z':
+      return (opponentScore + 1) % PLAYER_MOVES.length;
+    default:
+      throw new Error('Unknown result');
+  }
 }
 
 module.exports = (data) => {
-    // split the lines into tuples
-    const gamePairs = data.map((row) => row.split(' '));
+  // split the lines into tuples
+  const gamePairs = data.map((row) => row.split(' '));
 
-    const score = gamePairs.reduce((sum, [opponent, player]) => {
-        const result = roundResult(opponent, player);
-        let roundScore = PLAYER_MOVES.indexOf(player) + 1;
-        switch (result) {
-            case 0:
-                roundScore += 3;
-                break;
-            case 1:
-                roundScore += 6;
-                break;
-        }
-        return sum + roundScore;
-    }, 0);
+  const score = gamePairs.reduce((sum, [opponent, player]) => {
+    const result = roundResult(opponent, player);
+    let roundScore = PLAYER_MOVES.indexOf(player) + 1;
+    switch (result) {
+      case 0:
+        roundScore += 3;
+        break;
+      case 1:
+        roundScore += 6;
+        break;
+      default:
+        // noop
+    }
+    return sum + roundScore;
+  }, 0);
 
-    const nextScore = gamePairs.reduce((sum, [opponent, outcome]) => {
-        const player = roundMove(opponent, outcome);
-        const result = PLAYER_MOVES.indexOf(outcome) - 1;
-        let roundScore = player + 1;
-        switch (result) {
-            case 0:
-                roundScore += 3;
-                break;
-            case 1:
-                roundScore += 6;
-                break;
-        }
-        return sum + roundScore;
-    }, 0);
-    return {
-        part1: score,
-        part2: nextScore,
-    };
-}
+  const nextScore = gamePairs.reduce((sum, [opponent, outcome]) => {
+    const player = roundMove(opponent, outcome);
+    const result = PLAYER_MOVES.indexOf(outcome) - 1;
+    let roundScore = player + 1;
+    switch (result) {
+      case 0:
+        roundScore += 3;
+        break;
+      case 1:
+        roundScore += 6;
+        break;
+      default:
+        // noop
+    }
+    return sum + roundScore;
+  }, 0);
+  return {
+    part1: score,
+    part2: nextScore,
+  };
+};
--- day-4/index.js
+++ day-4/index.js
@@ -1,22 +1,21 @@
 module.exports = (data) => {
-    const [part1, part2] = data.reduce(([overlap, encompass], pair) => {
-        // clean the data into start/end pairs of ints, each pair ordered by the interval length
-        const [first, second] = pair.split(',').map((pair) => {
-            return pair.split('-').map((v) => parseInt(v, 10));
-        }).sort((a, b) => {
-            return (a[1] - a[0]) - (b[1] - b[0]);
-        });
-        // first is shortest in length so can only be encompassed by second
-        if (first[0] >= second[0] && first[1] <= second[1]) {
-            encompass++;
-            overlap++;
-        } else if (first[0] >= second[0] && first[0] <= second[1] || first[1] >= second[0] && first[1] <= second[1]) {
-            overlap++;
-        }
-        return [overlap, encompass];
-    }, [0, 0]);
-    return {
-        part1,
-        part2,
-    };
+  let overlap = 0;
+  let encompass = 0;
+  for (let i = 0; i < data.length; i += 1) {
+    const [first, second] = data[i].split(',')
+      .map((pair) => pair.split('-').map((val) => parseInt(val, 10)))
+      .sort((a, b) => (a[1] - a[0]) - (b[1] - b[0]));
+    // first is the shortest so can only be encompassed by second
+    if ((first[0] >= second[0] && first[0] <= second[1])
+        || (first[1] >= second[0] && first[1] <= second[1])) {
+      overlap += 1;
+      if (first[0] >= second[0] && first[1] <= second[1]) {
+        encompass += 1;
+      }
+    }
+  }
+  return {
+    part1: encompass,
+    part2: overlap,
+  };
 };
