f5bcac9b50433b1d9468059ba2dadc40698e8d96
Dan Hensby <git@hens.by>
2023-12-04 09:17:32 +0000

    feat: day 4 part 2

--- day-04/index.js
+++ day-04/index.js
@@ -6,20 +6,26 @@ function parseInput (input) {
   // lots of string bashing - regex would probably have been easier
   return input.map((line) => {
     const [card, info] = line.trim().split(':')
-    const id = parseInt(card.split(' ')[1].trim(), 10)
-    const [winning, result] = info.trim().split('|').map((nums) => nums.trim().split(' '))
+    const id = parseInt(card.split(/\s+/)[1].trim(), 10)
+    const [winning, result] = info.trim().split('|').map((nums) => nums.trim().split(/\s+/))
     return {
       id,
-      winning: winning.filter((v) => v !== '').map((num) => parseInt(num, 10)),
-      result: result.filter((v) => v !== '').map((num) => parseInt(num, 10)),
-      get score () {
-        return this.result.reduce((score, num) => {
+      winning: winning.map((num) => parseInt(num, 10)),
+      result: result.map((num) => parseInt(num, 10)),
+      get matches () {
+        return this.result.reduce((matches, num) => {
           if (this.winning.includes(num)) {
-            if (!score) {
-              score = 1
-            } else {
-              score *= 2
-            }
+            matches.push(num)
+          }
+          return matches
+        }, [])
+      },
+      get score () {
+        return this.matches.reduce((score) => {
+          if (!score) {
+            score = 1
+          } else {
+            score *= 2
           }
           return score
         }, 0)
@@ -33,6 +39,19 @@ module.exports = {
     const data = parseInput(input)
     return data.reduce((sum, { score }) => sum + score, 0)
   },
-  part2: (data) => {
+  part2: (input) => {
+    const data = parseInput(input)
+    // we create a list of all the cards - we will push duplicates onto the end
+    // until all duplicates have been dealt with
+    const list = [...data]
+    // loop over the list until we hit the end (the list will grow as we add duplicates)
+    for (let i = 0; i < list.length; i += 1) {
+      const game = list[i]
+      // find the next N games where N is the current game position + num of matches it has
+      for (let j = game.id; j < game.id + game.matches.length && j <= data.length; j += 1) {
+        list.push(data[j])
+      }
+    }
+    return list.length
   }
 }
