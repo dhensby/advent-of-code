58c3cece17b905b9b69ac8f665300941091ec9ed
Dan Hensby <git@hens.by>
2023-12-08 09:02:28 +0000

    feat: day 8 part 2 test working

--- day-08/index.js
+++ day-08/index.js
@@ -6,7 +6,7 @@ function parseInput (input) {
   const instructions = input.shift().trim().split('')
   input.shift()
   const elements = input.reduce((els, next) => {
-    const parsed = next.trim().match(/([A-Z]+)\s+=\s+\(([A-Z]+),\s+([A-Z]+)\)/)
+    const parsed = next.trim().match(/([0-9A-Z]+)\s+=\s+\(([0-9A-Z]+),\s+([0-9A-Z]+)\)/)
     return {
       ...els,
       [parsed[1]]: [parsed[2], parsed[3]]
@@ -24,15 +24,31 @@ function parseInput (input) {
         i += 1
       } while (currentPos !== 'ZZZ')
       return i
+    },
+    parallelTraverse () {
+      let positions = Object.keys(this.elements).filter((el) => {
+        return el[2] === 'A'
+      })
+      let i = 0
+      do {
+        const side = this.instructions[i % this.instructions.length] === 'L' ? 0 : 1
+        positions = positions.map((pos) => {
+          return this.elements[pos][side]
+        })
+        i += 1
+      } while (!positions.every((pos) => pos[2] === 'Z'))
+      return i
     }
   }
 }
 
 module.exports = {
   part1: (input) => {
-    const data = parseInput(input)
-    return data.traverse()
+    // const data = parseInput([...input])
+    // return data.traverse()
   },
-  part2: (data) => {
+  part2: (input) => {
+    const data = parseInput([...input])
+    return data.parallelTraverse()
   }
 }
