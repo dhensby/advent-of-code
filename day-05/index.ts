72188f2034a87f4beee87f7f3b105844b5ac149b
Dan Hensby <git@hens.by>
2024-12-05 11:03:43 +0000

    feat: solve day-5 part-2

--- day-05/index.ts
+++ day-05/index.ts
@@ -32,5 +32,34 @@ export async function part1 (raw: string[]): Promise<string> {
 }
 
 export async function part2 (raw: string[]): Promise<string> {
-  return ''
+  const [orderingRules, pages] = prepData(raw)
+  // optimise by doing a "graph" of the sorting rules
+  const graph: Record<number, number[]> = orderingRules.reduce((map, pair) => {
+    if (map[pair[0]] === undefined) {
+      map[pair[0]] = []
+    }
+    map[pair[0]].push(pair[1])
+    return map
+  }, Object.create(null))
+  console.log(graph)
+  return pages.reduce((accum, page, i) => {
+    const ordered = page.every((num, i, arr) => {
+      // are all the remaining elements allowed to come next
+      return arr.slice(i + 1).every((r) => graph[num]?.includes(r))
+    })
+    if (!ordered) {
+      // reorder them
+      page.sort((a, b) => {
+        if (a === b) return 0
+        // if the graph includes the number, it must come after
+        if (graph[b]?.includes(a)) {
+          return 1
+        }
+        // not included in the graph means it comes before
+        return -1
+      })
+      accum += page[Math.floor(page.length / 2)]
+    }
+    return accum
+  }, 0).toString(10)
 }
