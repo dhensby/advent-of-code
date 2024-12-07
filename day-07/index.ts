06bd0ef70c9f23772a478bc74e8d9f9b7bdc4d99
Dan Hensby <git@hens.by>
2024-12-07 18:02:26 +0000

    feat: solve day-7 part-2

--- day-07/index.ts
+++ day-07/index.ts
@@ -16,9 +16,6 @@ export async function part1 (raw: string[]): Promise<string> {
         if (candidate === undefined) {
           return sum
         }
-        if (candidate === answer) {
-          return sum + answer
-        }
         answers.push(candidate + parts[i])
         answers.push(candidate * parts[i])
       } while (potentialAnswers.length > 0)
@@ -33,6 +30,29 @@ export async function part1 (raw: string[]): Promise<string> {
 }
 
 export async function part2 (raw: string[]): Promise<string> {
-  // const data = prepData(raw)
-  return ''
+  const data = prepData(raw)
+  const result = data.reduce((sum, [answer, parts]) => {
+    let potentialAnswers = [parts[0]]
+    for (let i = 1; i < parts.length; i += 1) {
+      const answers: number[] = []
+      do {
+        const candidate = potentialAnswers.pop()
+        if (candidate === undefined) {
+          return sum
+        }
+        if (candidate > answer) {
+          continue
+        }
+        answers.push(candidate + parts[i])
+        answers.push(candidate * parts[i])
+        answers.push(parseInt(`${candidate.toString(10)}${parts[i].toString(10)}`, 10))
+      } while (potentialAnswers.length > 0)
+      potentialAnswers = answers
+    }
+    if (potentialAnswers.some((a) => a === answer)) {
+      return sum + answer
+    }
+    return sum
+  }, 0)
+  return result.toString(10)
 }
