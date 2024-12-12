2235995a707ed53368981f567d0ab465a0972636
Dan Hensby <git@hens.by>
2024-12-12 02:24:22 +0000

    feat: solve day-11 part-2

--- day-11/index.ts
+++ day-11/index.ts
@@ -29,6 +29,46 @@ export async function part1 (raw: string[]): Promise<string> {
   return res.length.toString(10)
 }
 
+function combine (l1: Array<[number, number]>): Array<[number, number]> {
+  return l1.reduce<Array<[number, number]>>((combined, [k, v], i) => {
+    const exists = combined.find(([num]) => {
+      return num === k
+    })
+    if (exists === undefined) {
+      combined.push([k, v])
+    } else {
+      exists[1] += v
+    }
+    return combined
+  }, [])
+}
+
+function blinkStone (number: number, count: number = 1): Array<[number, number]> {
+  if (number === 0) { return [[1, count]] }
+  const val = number.toString(10)
+  if (val.length % 2 === 0) {
+    const s1 = parseInt(val.substring(0, val.length / 2))
+    const s2 = parseInt(val.substring(val.length / 2))
+    // we could try to combine these two, but it's not really needed
+    return [[s1, count], [s2, count]]
+  } else {
+    return [[number * 2024, count]]
+  }
+}
+
 export async function part2 (raw: string[]): Promise<string> {
-  return ''
+  const data = prepData(raw).map<[number, number]>((num) => [num, 1])
+  let res: Array<[number, number]> = [...data]
+  for (let i = 0; i < 75; i += 1) {
+    const len = res.length
+    for (let j = 0; j < len; j += 1) {
+      const next = res.shift()
+      if (next === undefined) throw new Error()
+      res.push(...blinkStone(...next))
+    }
+    res = combine(res)
+  }
+  return res.reduce((sum, [, count]) => {
+    return sum + count
+  }, 0).toString(10)
 }
